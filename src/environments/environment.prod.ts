0db432f6fce6857a8613341674335d0a1f4109fe
itzvenkat <itzvenkat7@gmail.com>
2021-11-22 12:58:19 +0530

    Initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'itz-url-shortener',
+    appId: '1:217522390392:web:63e9ee5ccdc08f6263f48d',
+    databaseURL: 'https://itz-url-shortener-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'itz-url-shortener.appspot.com',
+    apiKey: 'AIzaSyAH57zTviKyGo1rz2jwN-6GWevLeIFqlvk',
+    authDomain: 'itz-url-shortener.firebaseapp.com',
+    messagingSenderId: '217522390392',
+    measurementId: 'G-8QE6Y1ZHFS',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'itz-url-shortener',
+    appId: '1:217522390392:web:63e9ee5ccdc08f6263f48d',
+    databaseURL: 'https://itz-url-shortener-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'itz-url-shortener.appspot.com',
+    apiKey: 'AIzaSyAH57zTviKyGo1rz2jwN-6GWevLeIFqlvk',
+    authDomain: 'itz-url-shortener.firebaseapp.com',
+    messagingSenderId: '217522390392',
+    measurementId: 'G-8QE6Y1ZHFS',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
